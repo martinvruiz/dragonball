808f241d3e5aff91017a3e08d741656126d10e94
martinvruiz <martinvruiz10@gmail.com>
2025-03-07 15:57:41 -0300

    fixed: all the text to spanish and added a second p on home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom"
 export default function Navbar() {
     return(
         <nav className="p-4">
-            <ul className="flex justify-center space-x-8 text-3xl text-black">
+            <ul className="flex justify-center space-x-8 text-xl md:text-2xl lg:text-3xl text-black">
                 <li className="hover:scale-125 transform transition duration-300">
-                    <Link to="/" >Home</Link>
+                    <Link to="/" >Inicio</Link>
                 </li>
                 <li className="hover:scale-125 transform transition duration-300">
-                    <Link to="/characters" >Characters</Link>
+                    <Link to="/characters" >Personajes</Link>
                 </li>
                 <li className="hover:scale-125 transform transition duration-300">
-                    <Link to="/planets" >planets</Link>
+                    <Link to="/planets" >Planetas</Link>
                 </li>
             </ul>
         </nav>
